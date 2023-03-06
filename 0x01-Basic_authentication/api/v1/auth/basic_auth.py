ef892fc36fa43d3e381c3d7712f80a2de107e87e
hanichris <hanichris71@gmail.com>
2023-03-07 00:28:30 +0300

    new type of authentication architecture.

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module defining a class to manage api authentication.
+
+This class serves as an extension of the base class `Auth.`
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    pass
