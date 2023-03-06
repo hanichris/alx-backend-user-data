3379ab254327e4edff09c238ffa163e10b445063
hanichris <hanichris71@gmail.com>
2023-03-07 00:23:00 +0300

    fix pycodestyle error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -41,9 +41,9 @@ def before_request():
     """Filter each request looking for authorization key."""
     if auth is not None:
         if auth.require_auth(request.path,
-                            ['/api/v1/status/',
-                            '/api/v1/unauthorized/',
-                            '/api/v1/forbidden/']):
+                             ['/api/v1/status/',
+                              '/api/v1/unauthorized/',
+                              '/api/v1/forbidden/']):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
