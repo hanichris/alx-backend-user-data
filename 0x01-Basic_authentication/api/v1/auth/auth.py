a2447fa277ba085ccbd1fed1b8e36013cdb4d060
hanichris <hanichris71@gmail.com>
2023-03-06 23:31:27 +0300

    add conditions for authentication requirement.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,8 +7,13 @@ from typing import List, TypeVar
 class Auth:
     """Manages API authentication."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """checks which endpoint requires authorization to access."""
-        return False
+        """checks which endpoint requires authentication to access."""
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = f"{path}/" if path[-1] != '/' else path
+        return not (path in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """Include the authorization header to the request being made."""
