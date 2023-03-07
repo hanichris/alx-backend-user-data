187c8de8a3ef9bf37de5fca04e42924577099882
hanichris <hanichris71@gmail.com>
2023-03-07 22:41:52 +0300

    Extract username and password from decoded base64 string.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ This class serves as an extension of the base class `Auth.`
 from .auth import Auth
 import base64
 import binascii
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -48,3 +49,21 @@ class BasicAuth(Auth):
             return None
         else:
             return decoded_value.decode('utf-8')
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """Return the username and email from a decoded base64 string.
+
+        Args:
+            decoded_base64_authorization_header (str): decode base64 string.
+        Return
+            tuple(str, str): username, password
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        values_list = decoded_base64_authorization_header.split(':')
+        if len(values_list) != 2:
+            return None, None
+        return values_list[0], values_list[1]
