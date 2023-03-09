fc4efcd796f6a20efd0e7821d728454c5ac04b8b
hanichris <hanichris71@gmail.com>
2023-03-09 13:00:08 +0300

    Create a new session authentication mechanism.
    Test operation of the session authentication mechanism using env variables.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if auth == "basic_auth":
 elif auth == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif auth == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module defining a class to manage api authentication.
+
+This class serves as an extension of the base class `Auth.`
+"""
+from auth import Auth
+
+
+class SessionAuth(Auth):
+    pass
