4b23e525d6d9b166bdea54055448c9a8e97c821c
hanichris <hanichris71@gmail.com>
2023-03-15 16:02:10 +0300

    create a session for a particular email address.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,19 @@ class Auth:
         except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode(), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a session id for the supplied email.
+
+        Args:
+            email (str): user's email address.
+        Return:
+            str: session id for the email.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        sess_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=sess_id)
+        return sess_id
