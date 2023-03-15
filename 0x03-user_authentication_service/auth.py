c1c8d2f0f45b512295063ca493d92a947f411518
hanichris <hanichris71@gmail.com>
2023-03-15 14:34:45 +0300

    Add a new user to the database if they did not already exist.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """Working with the `db.py` module"""
+from sqlalchemy.orm.exc import NoResultFound
+
 import bcrypt
+from typing import TypeVar
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +19,28 @@ def _hash_password(password: str) -> bytes:
     """
     password = password.encode()
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Create a new user while enforcing unique email addresses.
+
+        Args:
+            email (str): user's email address.
+            password (str): user's password.
+        Return:
+            User: the created user.
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_pwd)
+            return new_user
+        raise ValueError(f'User {email} already exists')
