6c556c4cffa96644758f65a6b5ff6cb934212da9
hanichris <hanichris71@gmail.com>
2023-03-16 21:33:09 +0300

    Set pool_pre_ping=True flag for pessimistic testing of connections in the pool.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -19,7 +19,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=False,
+                                     pool_pre_ping=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
