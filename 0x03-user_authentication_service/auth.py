b59173fc66132338e28e39404f70179eb7317a5d
hanichris <hanichris71@gmail.com>
2023-03-16 16:44:35 +0300

    reset a user's password token.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,3 +109,19 @@ class Auth:
             user_id (int): user's id.
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Update the user's `reset_token` database field.
+
+        Args:
+            email (str): user's email address.
+        Return:
+            str: reset token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
