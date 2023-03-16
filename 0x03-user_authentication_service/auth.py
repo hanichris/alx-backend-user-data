f75a961ca2ecd66c70d9cddc876d7372ff792cb1
hanichris <hanichris71@gmail.com>
2023-03-16 16:58:53 +0300

    Update a user's password and reset their reset_token to None.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -125,3 +125,19 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password.
+
+        Args:
+            reset_token (str): token to identify the user.
+            password (str): user's new password.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_pwd = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_pwd,
+                             reset_token=None)
