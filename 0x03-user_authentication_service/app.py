2a76a6334c808338363e05e8cd03d688a2e72051
hanichris <hanichris71@gmail.com>
2023-03-16 16:32:27 +0300

    Get the profile of a particular user.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,5 +50,15 @@ def logout_user():
     return redirect(url_for('index'))
 
 
+@app.route('/profile', strict_slashes=False)
+def display_profile():
+    """GET /profile"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": f"{user.email}"})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
